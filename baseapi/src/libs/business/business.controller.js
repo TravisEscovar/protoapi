6ffcaa7ce18d7667d2bd7dfa73c405827e3e5d97
Travis <41177668+TravisEscovar@users.noreply.github.com>
2021-10-14 09:18:59 -0600

    Update business.controller.js

--- baseapi/src/libs/business/business.controller.js
+++ baseapi/src/libs/business/business.controller.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 exports.findBusinessByGeneral = (req, res) => {
-    BusinessModel.findBusinessByGeneral(req.params.business, req.params.business, req.params.business, req.params.business)
+    BusinessModel.findBusinessByGeneral(req.params.business, req.params.business, req.params.business, req.params.business, req.params.etc, req.params.etc)
         .then((result) => {
             res.status(200).send(result);
         }, err => {
